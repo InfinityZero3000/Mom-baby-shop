68b1ea50fd6eee92baace74e7e068fad7d3cdfda
2001230909@hufi.edu.vn <nhthang312@hotmail.com>
2025-05-31 09:35:25 +0700

    Fix deployment issues: switch to HashRouter and simplify workflow

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { App } from "./App";
 import "./lib/utils";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter basename="/Mom-baby-shop">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
